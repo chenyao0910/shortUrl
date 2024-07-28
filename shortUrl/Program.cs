cc7020b0f79c264170b9bdf1df2dd2c52135124e
chenyao0910 <chenyao0910@smail.nchu.edu.tw>
2024-07-29 00:32:24 +0800

    add db connection string in appsetting

--- shortUrl/Program.cs
+++ shortUrl/Program.cs
@@ -1,6 +1,3 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using shortUrl.Interfaces;
 using shortUrl.Services;
 
@@ -11,7 +8,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
-
 builder.Services.AddTransient<IShortUrlService, ShortUrlService>();
 var app = builder.Build();
 
